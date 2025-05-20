4b54b09e00e32fe470d4ecd7a41f3c0f3875037f
vivek <vr7064300@gmail.com>
2025-05-20 23:25:05 +0530

    added: project setup files

--- postcss.config.mjs
+++ postcss.config.mjs
@@ -1,5 +1,14 @@
-const config = {
-  plugins: ["@tailwindcss/postcss"],
-};
+// const config = {
+//   plugins: ["@tailwindcss/postcss"],
+// };
 
+// export default config;
+
+// add from daisyui site
+// /** @type {import('postcss-load-config').Config} */
+const config = {
+  plugins: {
+    '@tailwindcss/postcss': {},
+  },
+};
 export default config;
